f6e61ee2788e838905be5637f18052373cd0b36c
Hong Seokho <hongseokhodev@gmail.com>
2026-07-22 15:06:10 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/pinlog/pinlogback/PinlogBackApplication.java
@@ -0,0 +1,13 @@
+package com.pinlog.pinlogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinlogBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinlogBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinlog/pinlogback/PinlogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinlog.pinlogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinlogBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
