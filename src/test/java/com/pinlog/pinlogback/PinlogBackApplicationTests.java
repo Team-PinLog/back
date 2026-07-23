bdaae576c8a39cf54bda04b4fcd4f4956ed7219a
MINYONG PARK <parkminyong22@gmail.com>
2026-07-23 22:45:05 +0900

    test: verify CI failure path

--- src/test/java/com/pinlog/pinlogback/PinlogBackApplicationTests.java
+++ src/test/java/com/pinlog/pinlogback/PinlogBackApplicationTests.java
@@ -21,4 +21,9 @@ class PinlogBackApplicationTests extends PostgresContainerSupport {
 	void contextLoads() {
 	}
 
+	@Test
+	void ciFailurePathProbe() {
+		throw new AssertionError("intentional CI failure-path probe");
+	}
+
 }
